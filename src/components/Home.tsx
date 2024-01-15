f2122641efa10350d923bedda7dea0881aea5e66
tittieth <tittie.thomasson@hotmail.se>
2024-01-15 11:50:04 +0100

    feat: navigate to weather page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,16 @@
 import { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const [name, setName] = useState<string>('');
   console.log(name);
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log('Form submitted:', name);
     localStorage.setItem('name', JSON.stringify(name));
+    navigate('/weather');
   };
 
   return (
