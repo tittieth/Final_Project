e7fe9e0887fe75791fbb17736081fe6888498b11
tittieth <tittie.thomasson@hotmail.se>
2024-01-15 11:46:18 +0100

    Add: html structure and form handling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,31 @@
+import { ChangeEvent, useState } from 'react';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [name, setName] = useState<string>('');
+  console.log(name);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Form submitted:', name);
+  };
+
+  return (
+    <div>
+      <h1>Superklädhjälten</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+            type="text"
+            name="name"
+            value={name}
+          />
+        </label>
+        <button type="submit">Start</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
