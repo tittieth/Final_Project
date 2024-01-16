4629a2732e2b68a86f49f98d94b52e28c58bbd0c
tittieth <tittie.thomasson@hotmail.se>
2024-01-16 11:03:43 +0100

    Add: servicefile for weather data

---
+++ src/services/WeatherService.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import IWeatherData from '../models/IWeather';
+
+const Base_URL = `https://api.openweathermap.org/data/2.5/weather`;
+
+export const getWeather = async (latitude: number, longitude: number): Promise<IWeatherData> => {
+  try {
+    const response = await axios.get<IWeatherData>(
+      `${Base_URL}?lat=${latitude}&lon=${longitude}&units=metric&appid=${import.meta.env.VITE_API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching data');
+  }
+};
