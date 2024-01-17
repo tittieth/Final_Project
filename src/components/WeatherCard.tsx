5c71550732ce5e7df8dfaed22738c62f79025700
tittieth <tittie.thomasson@hotmail.se>
2024-01-17 09:40:36 +0100

    Feat: show custom icons for weather

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -5,10 +5,30 @@ type Props = {
 };
 
 const WeatherCard = ({ weatherData }: Props) => {
+  const customIconMappings: Record<string, string> = {
+    Snow: 'snow.png',
+    Clear: 'clear-sky.png',
+    Clouds: 'scattered-clouds.png',
+    Rain: 'rain.png',
+    Thunderstorm: 'thunderstorm.png',
+  };
+
+  const customIcon = customIconMappings[weatherData.weather[0].main];
+
   return (
     <div>
       <h1>Väder i {weatherData.name}</h1>
       <h2>{weatherData.weather[0].main}</h2>
+      <img
+        src={`/public/weather-icons/${customIcon}`}
+        alt={weatherData.weather[0].description}
+        height={50}
+        width={50}
+      />
+      <img
+        src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+        alt={weatherData.weather[0].description}
+      />
       <p>Temperatur: {Math.round(weatherData.main.temp)} °C</p>
     </div>
   );
