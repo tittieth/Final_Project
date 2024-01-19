7f4f90edd25cc4b9408f5e7ee339beef7e95dc60
tittieth <tittie.thomasson@hotmail.se>
2024-01-19 14:23:16 +0100

    Add: navigation component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Navigation from './Navigation';
 
 const Layout = () => {
   return (
@@ -6,6 +7,7 @@ const Layout = () => {
       <main>
         <Outlet></Outlet>
       </main>
+      <Navigation></Navigation>
     </>
   );
 };
