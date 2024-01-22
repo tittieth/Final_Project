23fe0b879e5885b82689d9678ee66cb1f877480d
tittieth <tittie.thomasson@hotmail.se>
2024-01-22 12:58:30 +0100

    Add: html structure

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,18 @@
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div>
+      <h1>Vilse?</h1>
+      <div>
+        <img src="/img/boy-with-umbrella.png" alt="3d boy walking with an umbrella" width={200} height={300} />
+      </div>
+      <div>
+        <p>
+          Vi kunde inte hitta sidan du försökte nå. Det kan bero på att sidan har flyttats eller att vi har tagit bort
+          den. Använd menyn nedan för att ta dig tillbaka.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
