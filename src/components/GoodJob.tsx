ef47634f79adb948f58824a0571b6f8d0195a110
tittieth <tittie.thomasson@hotmail.se>
2024-01-22 11:11:27 +0100

    Add: star icon

--- src/components/GoodJob.tsx
+++ src/components/GoodJob.tsx
@@ -5,7 +5,34 @@ const GoodJob = () => {
 
   return (
     <div>
-      <h1>Bra jobbat {name}</h1>
+      <h1>Bra jobbat {name}!</h1>
+      <div>
+        <img
+          src="/public/img/casual-life-3d-yellow-star.png"
+          alt="casual-life-3d-yellow-star.png"
+          width={50}
+          height={50}
+        />
+        <img
+          src="/public/img/casual-life-3d-yellow-star.png"
+          alt="casual-life-3d-yellow-star.png"
+          width={50}
+          height={50}
+        />
+        <img
+          src="/public/img/casual-life-3d-yellow-star.png"
+          alt="casual-life-3d-yellow-star.png"
+          width={50}
+          height={50}
+        />
+        <img
+          src="/public/img/casual-life-3d-yellow-star.png"
+          alt="casual-life-3d-yellow-star.png"
+          width={50}
+          height={50}
+        />
+      </div>
+      <p>Nu är du redo att gå ut! Ha så kul!</p>
     </div>
   );
 };
