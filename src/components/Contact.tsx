3631cc2ecf1f647f583955798f7a378daed614b4
tittieth <tittie.thomasson@hotmail.se>
2024-01-22 09:19:35 +0100

    Add: navigation with styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,6 +4,7 @@ import submitForm from '../services/ContactFormService';
 import { useFormik } from 'formik';
 import { validationSchema } from '../models/ValidationSchema';
 import { FormikHelpers } from 'formik';
+import Navigation from './Navigation';
 
 export type FormValues = {
   name: string;
@@ -36,65 +37,68 @@ const Contact = () => {
   const [confirmation, setConfirmation] = useState(false);
 
   return (
-    <div className="contact-wrapper">
-      <h1>Kontakt</h1>
-      <p className="contact-info">Har du frågor eller funderingar? Skicka oss ett meddelande!</p>
-      <form onSubmit={handleSubmit}>
-        <label className="input-wrapper">
-          Namn:
-          <input
-            type="text"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.name && touched.name ? 'input-error' : ''}
-          />
-        </label>
-        <div className="error">{errors.name && touched.name && <p>{errors.name}</p>}</div>
+    <>
+      <div className="contact-wrapper">
+        <h1>Kontakt</h1>
+        <p className="contact-info">Har du frågor eller funderingar? Skicka oss ett meddelande!</p>
+        <form onSubmit={handleSubmit}>
+          <label className="input-wrapper">
+            Namn:
+            <input
+              type="text"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={errors.name && touched.name ? 'input-error' : ''}
+            />
+          </label>
+          <div className="error">{errors.name && touched.name && <p>{errors.name}</p>}</div>
 
-        <label className="input-wrapper">
-          Mejladress:
-          <input
-            type="email"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.email && touched.email ? 'input-error' : ''}
-          />
-        </label>
-        {errors.email && touched.email && <p className="error">{errors.email}</p>}
+          <label className="input-wrapper">
+            Mejladress:
+            <input
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={errors.email && touched.email ? 'input-error' : ''}
+            />
+          </label>
+          {errors.email && touched.email && <p className="error">{errors.email}</p>}
 
-        <label className="input-wrapper">
-          Meddelande:
-          <textarea
-            name="message"
-            value={values.message}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.message && touched.message ? 'input-error' : ''}
-          />
-        </label>
-        {errors.message && touched.message && <p className="error">{errors.message}</p>}
+          <label className="input-wrapper">
+            Meddelande:
+            <textarea
+              name="message"
+              value={values.message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={errors.message && touched.message ? 'input-error' : ''}
+            />
+          </label>
+          {errors.message && touched.message && <p className="error">{errors.message}</p>}
 
-        <div>
-          <button type="submit" disabled={isSubmitting}>
-            Skicka
-          </button>
+          <div>
+            <button type="submit" disabled={isSubmitting}>
+              Skicka
+            </button>
+          </div>
+        </form>
+        <div className="contact-img-wrapper">
+          <img
+            className="contact-img"
+            src="/public/img/casual-life-3d-young-woman-in-headphones-sitting-in-front-of-laptop-and-waving.png"
+            alt="3d woman in headphones sitting in front of laptop"
+            width={130}
+            height={130}
+          />
         </div>
-      </form>
-      <div className="contact-img-wrapper">
-        <img
-          className="contact-img"
-          src="/public/img/casual-life-3d-young-woman-in-headphones-sitting-in-front-of-laptop-and-waving.png"
-          alt="3d woman in headphones sitting in front of laptop"
-          width={130}
-          height={130}
-        />
+        {confirmation && <ConfirmationModal setConfirmation={setConfirmation}></ConfirmationModal>}
       </div>
-      {confirmation && <ConfirmationModal setConfirmation={setConfirmation}></ConfirmationModal>}
-    </div>
+      <Navigation></Navigation>
+    </>
   );
 };
 
