72b107833514ed9f4e1e17042e3629365f0455ac
tittieth <tittie.thomasson@hotmail.se>
2024-01-26 17:21:41 +0100

    Cypress: add tests for start page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('start page', () => {
+  it('Should load start page', () => {
+    cy.visit('http://localhost:5174/');
+  });
+
+  it('Should show the application title', () => {
+    cy.visit('http://localhost:5174/');
+    cy.contains('Superklädhjälten');
+  });
+
+  it('Should submit if name is valid', () => {
+    cy.visit('http://localhost:5174/');
+    cy.get('input[name="name"]').type('John');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/weather');
+  });
+
+  it('Should show error message when name is empty', () => {
+    cy.visit('http://localhost:5174/');
+    cy.get('button[type="submit"]').click();
+    cy.get('.error').should('be.visible').and('contain', 'Detta fält är obligatoriskt. Vänligen fyll i det');
+  });
+});
