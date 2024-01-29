a99ac438cd48b2cf6bed3a1baacb0a6f0826b7b5
tittieth <tittie.thomasson@hotmail.se>
2024-01-29 10:50:47 +0100

    Cypress: add tests for weather page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,23 +1,35 @@
 describe('start page', () => {
   it('Should load start page', () => {
-    cy.visit('http://localhost:5174/');
+    cy.visit('/');
   });
 
   it('Should show the application title', () => {
-    cy.visit('http://localhost:5174/');
+    cy.visit('/');
     cy.contains('Superklädhjälten');
   });
 
   it('Should submit if name is valid', () => {
-    cy.visit('http://localhost:5174/');
+    cy.visit('/');
     cy.get('input[name="name"]').type('John');
     cy.get('button[type="submit"]').click();
     cy.url().should('include', '/weather');
   });
 
   it('Should show error message when name is empty', () => {
-    cy.visit('http://localhost:5174/');
+    cy.visit('/');
     cy.get('button[type="submit"]').click();
     cy.get('.error').should('be.visible').and('contain', 'Detta fält är obligatoriskt. Vänligen fyll i det');
+  });
+});
+
+describe('weather page', () => {
+  it('successfully loads', () => {
+    cy.visit('/weather');
+    cy.wait(4000);
+
+    cy.get('h1').should('contain', 'Hej');
+    cy.get('h3').should('have.text', 'Du behöver klä på dig:');
+
+    cy.get('.clothing-cards-wrapper').children().should('have.length.greaterThan', 0);
   });
 });
