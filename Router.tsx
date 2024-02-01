25e7cbc6b684a933b6c772f3c9e605ddee33796b
tittieth <tittie.thomasson@hotmail.se>
2024-02-01 14:59:53 +0100

    Update: change to hashrouter

--- Router.tsx
+++ Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import Layout from './src/components/Layout';
 import Error from './src/components/Error';
 import Home from './src/components/Home';
@@ -7,7 +7,7 @@ import Weather from './src/components/Weather';
 import GoodJob from './src/components/GoodJob';
 import AboutPage from './src/components/AboutPage';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <Layout></Layout>,
