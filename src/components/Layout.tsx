cc29dd01d6ea0a792aa07dc2643526c5dde0a6a9
tittieth <tittie.thomasson@hotmail.se>
2024-02-02 14:35:34 +0100

    Fix: scroll to top on page change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,22 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Navigation from './Navigation';
+import { useEffect } from 'react';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return <></>;
+};
 
 const Layout = () => {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
   return (
     <>
+      <ScrollToTop></ScrollToTop>
       <main>
         <Outlet></Outlet>
       </main>
